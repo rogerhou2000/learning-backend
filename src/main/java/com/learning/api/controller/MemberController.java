d2392d39f98b1a3bedd4167c0dcaddc24b1d9b42
lianne928 <lianne.14928@gmail.com>
2026-03-02 07:47:25 +0800

    feat: implement login API

--- src/main/java/com/learning/api/controller/MemberController.java
+++ src/main/java/com/learning/api/controller/MemberController.java
@@ -4,13 +4,11 @@ import com.learning.api.entity.Member;
 import com.learning.api.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/auth")
 public class MemberController {
