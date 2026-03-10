71302f30318e2e100fc15339de9e9c85475a6a83
rogerhou2000 <rogerhou2000@gmail.com>
2026-03-10 23:33:27 +0800

    refactor: 調整 Tutor 類別中的證照欄位，新增多個 certificate_1 和 certificate_name_1，並移除舊的 certificate 欄位

--- src/main/java/com/learning/api/entity/Tutor.java
+++ src/main/java/com/learning/api/entity/Tutor.java
@@ -18,18 +18,32 @@ import lombok.Setter;
 public class Tutor {
     @Id
     private Long id;
+    @Column(length = 50)
+    private String title;
+
+    @Column(length = 500)
+    private String avachar; //大頭照url
 
     @Column(length = 1000)
     private String intro;
 
-    @Column(length = 500)
-    private String certificate;
+    @Column(name ="certificate_1" ,length = 500)
+    private String certificate_1;//位址
+
+    @Column(name ="certificate_name_1" ,length = 500)
+    private String certificate_name_1;//證照名稱
+
+    @Column(name ="certificate_2" ,length = 500)
+    private String certificate_2;//位址
+    
+    @Column(name ="certificate_name_2" ,length = 500)
+    private String certificate_name_2;//證照名稱
 
     @Column(name = "video_url_1", length = 500)
-    private String videoUrl1;
+    private String videoUrl1; //自我介紹影片url
 
     @Column(name = "video_url_2", length = 500)
-    private String videoUrl2;
+    private String videoUrl2;//試教介紹影片url
 
     @Column(name = "bank_code", length = 10)
     private String bankCode;
