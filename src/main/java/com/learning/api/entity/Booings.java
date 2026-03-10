89e2c655ec8b9b335dedb8419b4057bd166afb7e
林伯罕 <u1062248@gmail.com>
2026-03-10 15:25:08 +0800

    feat: 在 Booings 類別中新增空行以改善代碼可讀性

--- src/main/java/com/learning/api/entity/Booings.java
+++ src/main/java/com/learning/api/entity/Booings.java
@@ -16,6 +16,7 @@ public class Booings {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    
     @Column(name = "order_id", nullable = false)
     private Long bookingId;
 
