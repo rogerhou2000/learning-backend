b91a419ce2b4755cfb880fd140fb415b73fd4725
林伯罕 <u1062248@gmail.com>
2026-03-19 15:45:17 +0800

    feat: 更新聊天訊息資料表，將角色欄位類型從整數改為字串，並調整註解內容

--- src/main/resources/demodb2.sql
+++ src/main/resources/demodb2.sql
@@ -47,7 +47,7 @@ CREATE TABLE `bookings` (
 CREATE TABLE `chat_messages` (
   `id` bigint(20) NOT NULL,
   `order_id` bigint(20) NOT NULL,
-  `role` tinyint(4) NOT NULL COMMENT '1=學生 2=老師',
+  `role` varchar(20) NOT NULL COMMENT 'student / tutor',
   `message` varchar(1000) NOT NULL,
   `message_type` tinyint(4) NOT NULL COMMENT '1.text 2sticker 貼圖',
   `media_url` varchar(500) DEFAULT NULL,
