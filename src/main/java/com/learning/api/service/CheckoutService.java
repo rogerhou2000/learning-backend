0b8b3a37c1074bd2d76cd8744496ebd2b67e4f2f
林伯罕 <u1062248@gmail.com>
2026-03-23 13:34:50 +0800

    refactor: 注釋掉未使用的 UUID 匯入

--- src/main/java/com/learning/api/service/CheckoutService.java
+++ src/main/java/com/learning/api/service/CheckoutService.java
@@ -15,7 +15,7 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
+/* import java.util.UUID; */
 
 @Service
 public class CheckoutService {
