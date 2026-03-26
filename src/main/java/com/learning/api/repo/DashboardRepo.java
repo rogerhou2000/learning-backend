d72e1a3223893ad58452a962416aee205d624c81
林伯罕 <u1062248@gmail.com>
2026-03-26 16:34:20 +0800

    移除不必要的註解導入，清理代碼

--- src/main/java/com/learning/api/repo/DashboardRepo.java
+++ src/main/java/com/learning/api/repo/DashboardRepo.java
@@ -1,6 +1,6 @@
 package com.learning.api.repo;
 
-import com.learning.api.dto.DashboardDTO.PopularCourseDTO;
+/* import com.learning.api.dto.DashboardDTO.PopularCourseDTO; */
 import com.learning.api.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
